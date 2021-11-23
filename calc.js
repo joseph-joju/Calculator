d1bd8643278f09288e5bbc49d8272844f912fbf6
Joseph Joju <joseph@neoito.com>
2021-11-23 15:08:34 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,18 @@
+function v(val){
+    document.getElementById("d").value+=val;
+}
+
+function c(val){
+    document.getElementById("d").value=val;
+}
+
+function e(){
+    try{
+        c(eval(document.getElementById("d").value))
+    }
+
+
+catch(e){
+    c('error')
+}
+}
---
+++ calculator.js
@@ -0,0 +1,29 @@
+function v(val){
+    document.getElementById("d").value+=val
+
+}
+
+function c(val){
+    document.getElementById("d").value=val
+
+}
+function plus(){
+    plusElem=parseInt(document.getElementById("d").value)
+
+}
+function multiply(){
+
+}
+function minus(){
+
+}
+
+
+function e(){
+    try{
+        if(plusElem){
+            document.getElementById("d").value=plusElem+
+        }
+    }
+
+}
